de77a01037c573ed39b34e60fc60a556dcfe6a41
Koon <gameskoon101020@gmail.com>
2023-09-18 14:33:20 +0700

    Update #2

--- index.js
+++ index.js
@@ -4,17 +4,32 @@ const port = 80
 const path = require('path')
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "pages"));
 
 app.get('/', (req, res) => {
   res.redirect("/watch")
 })
 
 app.get('/watch', (req, res) => {
-    if (req.query["v"] != undefined) {
-        res.sendFile(path.join(__dirname, "/pages/watch.html"))
-    }else {
-        res.redirect("/watch?v=bHQqvYy5KYo")
+  if (req.query["v"] != undefined) {
+    async function testt() {
+      let testtt = await fetch(`https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=${req.query["v"]}`)
+      let result = await testtt.json()
+      console.log(result)
+      res.render("watch", {
+        "title":result.title,
+        "thumnail":`https://i.ytimg.com/vi/${req.query["v"]}/maxresdefault.jpg`
+      })
     }
+    testt()
+  } else {
+    res.redirect("/watch?v=bHQqvYy5KYo")
+  }
+})
+
+app.get("/*", (req,res) => {
+  res.redirect("/")
 })
 
 app.listen(port, () => {
